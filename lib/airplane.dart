d09e3f2d647ffcb03289ebdda060b4822da90bf3
prabjot <prabjot24x7@gmail.com>
2024-08-06 14:15:03 -0400

    Added encrypted preferences

--- lib/airplane.dart
+++ lib/airplane.dart
@@ -16,4 +16,26 @@ class Airplane {
     required this.maxSpeed,
     required this.range,
   });
+
+  // Convert an Airplane object into a Map object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type,
+      'numberOfPassengers': numberOfPassengers,
+      'maxSpeed': maxSpeed,
+      'range': range,
+    };
+  }
+
+  // Convert a Map object into an Airplane object
+  factory Airplane.fromJson(Map<String, dynamic> json) {
+    return Airplane(
+      id: json['id'] as int?,
+      type: json['type'] as String,
+      numberOfPassengers: json['numberOfPassengers'] as int,
+      maxSpeed: json['maxSpeed'] as double,
+      range: json['range'] as double,
+    );
+  }
 }
